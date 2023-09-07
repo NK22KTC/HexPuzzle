19b664c0d3d4b0a4e9c8c2458ceab2e562898608
NK22KTC <ktc2231020001@edu.kyoto-tech.ac.jp>
2023-09-08 03:03:41 +0900

    <バグあり> オブジェクトが滑らかに回転するようにしました。

--- Assets/ItemController.cs
+++ Assets/ItemController.cs
@@ -8,14 +8,19 @@ public class ItemController : MonoBehaviour
 {
     PlayerInput playerInput;
 
-    
-    private Vector2 screenMousePosition;
+
     private Vector3 worldMousePosition;
 
     private InputAction.CallbackContext mouseContext;
 
     private GameObject movement_Item, movement_Item_Parent;
     private Vector3 itemLocalPos, itemRotation;
+
+    private float rotationTime = 0;
+    private bool isRotation = false;
+
+    enum RotationMode { Left, Right };
+    RotationMode rotationMode;
 
     void Start()
     {
@@ -26,6 +31,8 @@ public class ItemController : MonoBehaviour
     void Update()
     {
         HoldingItemControl();
+
+        SmoothlyRotate();
     }
 
     // マウス座標が更新された時に通知するコールバック関数
@@ -36,6 +43,8 @@ public class ItemController : MonoBehaviour
 
     public void OnHoldItem(InputAction.CallbackContext context)
     {
+        if (isRotation) return;
+
         if (context.phase == InputActionPhase.Started)
         {
             RaycastHit2D hit = Physics2D.Raycast(worldMousePosition, Vector2.zero);
@@ -56,26 +65,30 @@ public class ItemController : MonoBehaviour
     public void OnRotateItem(InputAction.CallbackContext context)
     {
         if (movement_Item_Parent == null) return;
+        if (isRotation) return;
 
         if(context.started)
         {
             if (Keyboard.current.aKey.wasPressedThisFrame)
             {
                 //反時計回りに回転させる
-                Debug.Log("aaaaaa");
-                itemRotation.z += 60;
-                if (itemRotation.z <= 0) itemRotation.z += 360;
-
-                movement_Item_Parent.transform.localEulerAngles = itemRotation;
+                itemRotation.z = (int)itemRotation.z + 60;
+                rotationMode = RotationMode.Left;
             }
             if (Keyboard.current.dKey.wasPressedThisFrame)
             {
                 //時計回りに回転させる
-                Debug.Log("dddddd");
-                itemRotation.z -= 60;
-                if (itemRotation.z >= 360) itemRotation.z -= 360;
+                itemRotation.z = (int)itemRotation.z - 60;  
+                rotationMode = RotationMode.Right;
+            }
 
-                movement_Item_Parent.transform.localEulerAngles = itemRotation;
+            if (itemRotation.z > 360)
+            {
+                itemRotation.z -= 360;
+            }
+            else if (itemRotation.z < 0)
+            {
+                itemRotation.z += 360;
             }
         }
     }
@@ -87,15 +100,70 @@ public class ItemController : MonoBehaviour
         if (mouseContext.phase is InputActionPhase.Started or InputActionPhase.Performed)
         {
             worldMousePosition.z = 0;
-            //movement_Item_Parent.transform.position = worldMousePosition - itemLocalPos;
-            float itemAngle = movement_Item_Parent.transform.localEulerAngles.z;
-            Debug.Log(itemAngle);
             movement_Item_Parent.transform.position = worldMousePosition;
         }
         else
         {
             movement_Item = null;
-            movement_Item_Parent = null;
+            if(!isRotation) movement_Item_Parent = null;
+        }
+    }
+
+    Vector3 oldRotation;
+    private float currentVelocity;
+    void SmoothlyRotate()
+    {
+        if (movement_Item_Parent == null) return;
+
+        if (movement_Item_Parent.transform.localEulerAngles != itemRotation)
+        {
+            if (!isRotation) oldRotation = movement_Item_Parent.transform.localEulerAngles;
+
+            Vector3 newRotation = oldRotation;
+
+            newRotation.z = Mathf.SmoothDampAngle(movement_Item_Parent.transform.localEulerAngles.z, itemRotation.z, ref currentVelocity, rotationTime, 1000f);
+            Debug.Log("Approximately : " + Mathf.Approximately(newRotation.z, itemRotation.z));  //ここバグる
+            if (Mathf.Approximately(newRotation.z, itemRotation.z))
+            {
+                movement_Item_Parent.transform.localEulerAngles = itemRotation;
+
+                if (movement_Item_Parent.transform.localEulerAngles.z <= 0)
+                {
+                    movement_Item_Parent.transform.localEulerAngles
+                        = new Vector3(0, 0, 360);
+                }
+                else if (movement_Item_Parent.transform.localEulerAngles.z >= 360)
+                {
+                    movement_Item_Parent.transform.localEulerAngles
+                        = new Vector3(0, 0, 0);
+                } 
+
+                isRotation = false;
+                return;
+            }
+            else
+            {
+                Debug.Log("newRotation.z : " + newRotation.z);
+                Debug.Log("itemRotation.z : " + itemRotation.z);
+            }
+
+            if(rotationMode == RotationMode.Left && newRotation.z >= 360)
+            {
+                newRotation.z = 0;
+            }
+            else if(rotationMode == RotationMode.Right && newRotation.z <= 0)
+            {
+                newRotation.z = 0;
+            }
+
+            movement_Item_Parent.transform.localEulerAngles = newRotation;
+            rotationTime += Time.deltaTime;
+            isRotation = true;
+        }
+        else if(rotationTime != 0)
+        {
+            rotationTime = 0;
+            isRotation = false;
         }
     }
 }
